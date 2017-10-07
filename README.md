62b8551c4b63c30e1ab827f7dceac9b4ec92c83d
Rodolfo Castillo <rvallada@ucsc.edu>
2017-10-06 20:44:45 -0700

    Source maintenance

---
+++ README.md
@@ -0,0 +1,8 @@
+# Leinglo's Avali Drone Pet 
+
+You can find the source code for both vanilla and PPETS here.
+
+All credit for this mod goes to Leinglo. However, I have done a few modifications:
+
+1. The drone doesn't follow as much as what seemed like 24/7.
+2. A custom pet behavior script for docking in the Avali pet house (but it isn't perfect).
---
+++ tools/pack.sh
@@ -0,0 +1,27 @@
+modname=""
+
+pak=$modname".pak"
+dir=$modname
+
+packer="asset_packer"
+starboundmoddir=/c/Program\ Files\ \(x86\)/Steam/steamapps/common/Starbound/mods
+
+if [[ $1 == "" ]]; then
+  echo "Specify vanilla or ppets"
+fi
+
+if [[ $1 == "vanilla" ]]; then
+  modname="avalidronepet"
+fi
+
+if [[ $1 == "ppets" ]]; then
+  modname="avalidronepet-ppets"
+fi
+
+mkdir "packed"
+command=$packer" ./"$dir" ./packed/"$pak
+eval ${command}
+
+if [[ $2 == "install" ]]; then
+  cp ./packed/$pak "$starboundmoddir/"$pak
+fi
