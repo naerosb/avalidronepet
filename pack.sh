0a8f2e16726cdfff1be02a5a24b0073524b4b51e
wolfchase <rcvallada@gmail.com>
2018-06-16 18:32:08 -0700

    Update pack.sh to enable installing

--- pack.sh
+++ pack.sh
@@ -5,6 +5,13 @@ if [[ $1 == "avalidronepet-ppets" ]]; then
     pak="avalidronepet-ppets.pak"
 fi
 
+starboundmoddir=/c/Program\ Files\ \(x86\)/Steam/steamapps/common/Starbound/mods
+
 mkdir "packed"
 command=$packer" ./"$1" ./packed/"$pak
 eval ${command}
+
+if [[ $2 == "install" ]]; then
+  cp ./packed/$pak "$starboundmoddir/"$pak
+fi
+
